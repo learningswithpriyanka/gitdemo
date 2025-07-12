fddeaf3aff17abb54359a037bf91970b339abe77
Priyanka Mahajan <ws_htu1587@hoonartek03.net>
2025-07-12 11:30:03 +0530

    Refactor index.js to improve structure and remove unused variables

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var a = 4;
+// This is a simple IIFE (Immediately Invoked Function Expression)
+(function greet (name){
+  console.log("Hello, " + name + "!");
+  // More code can be added here
+  // For example, we can log the value of s
+  console.log("The value of s is: " + s);
+})("world");
 
-function demo (){
-  var a = 10;
-  console.log("a: " + a);
+function s() {
+  return "This is a simple function.";
 }
-console.log("a: is nothing" + a);
-console.log("a: is nothing but IDK" + a);
 
-//let vs var vs const demonstration
+//arrow function example
+const arrowGreet = (name) => {
+  console.log("Hello from arrow function, " + name + "!");
+}
+
+//function expression example
+const functionExpressionGreet = function(name) {
+  console.log("Hello from function expression, " + name + "!");
+};
+
+//anonymous function example
+const anonymousGreet = function(name) {
+  console.log("Hello from anonymous function, " + name + "!");
+};
+
+
